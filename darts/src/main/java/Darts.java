d16a7c872602cb4563baf27879083121beb10871
jmathieu-riis <jmathieu@riis.com>
2019-10-30 12:01:28 -0400

    completed constructor and added distFromOrigin function

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,6 +1,18 @@
 class Darts {
+    // Darts class variables
+    private double x;
+    private double y;
 
     Darts(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Calculate distance of x, y coordinate from origin (0, 0)
+     */
+    private double distFromOrigin() {
+        
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
