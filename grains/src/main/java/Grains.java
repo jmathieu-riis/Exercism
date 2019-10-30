34b9901ced990909dae138e38e66f91a55ba8cb9
jmathieu-riis <jmathieu@riis.com>
2019-10-30 17:00:43 -0400

    solved the GRAINS problem

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -2,12 +2,25 @@ import java.math.BigInteger;
 
 class Grains {
 
-    BigInteger computeNumberOfGrainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    /**
+     * Calculate total number of grains for the given square between 1 and 64 (inclusively)
+     **/
+    BigInteger computeNumberOfGrainsOnSquare(final int square) throws IllegalArgumentException {
+        if (square < 1 || square > 64) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return BigInteger.TWO.pow(square - 1);
     }
 
+    /**
+     * Count total number of grains on a board with 64 squares
+     **/
     BigInteger computeTotalNumberOfGrainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        BigInteger totalGrains = BigInteger.ZERO;
+        for (int i = 1; i <= 64; ++i) {
+            totalGrains = totalGrains.add(computeNumberOfGrainsOnSquare(i));
+        }
+        return totalGrains;
     }
 
 }
--- grains/src/test/java/GrainsTest.java
+++ grains/src/test/java/GrainsTest.java
@@ -29,49 +29,49 @@ public class GrainsTest {
         assertEquals(new BigInteger("1"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void countAtSquare2() {
         BigInteger result = grains.computeNumberOfGrainsOnSquare(2);
         assertEquals(new BigInteger("2"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void countAtSquare3() {
         BigInteger result = grains.computeNumberOfGrainsOnSquare(3);
         assertEquals(new BigInteger("4"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void countAtSquare4() {
         BigInteger result = grains.computeNumberOfGrainsOnSquare(4);
         assertEquals(new BigInteger("8"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void countAtSquare16() {
         BigInteger result = grains.computeNumberOfGrainsOnSquare(16);
         assertEquals(new BigInteger("32768"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void countAtSquare32() {
         BigInteger result = grains.computeNumberOfGrainsOnSquare(32);
         assertEquals(new BigInteger("2147483648"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void countAtSquare64() {
         BigInteger result = grains.computeNumberOfGrainsOnSquare(64);
         assertEquals(new BigInteger("9223372036854775808"), result);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void errorOnNullBoardSize() {
         expectedException.expect(IllegalArgumentException.class);
@@ -79,7 +79,7 @@ public class GrainsTest {
         grains.computeNumberOfGrainsOnSquare(0);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void errorOnNegativeBoardSize() {
         expectedException.expect(IllegalArgumentException.class);
@@ -87,7 +87,7 @@ public class GrainsTest {
         grains.computeNumberOfGrainsOnSquare(-1);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void errorOnExcessiveBoardSize() {
         expectedException.expect(IllegalArgumentException.class);
@@ -95,7 +95,7 @@ public class GrainsTest {
         grains.computeNumberOfGrainsOnSquare(65);
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void totalNumberOfGrainsOnABoard() {
         BigInteger total = grains.computeTotalNumberOfGrainsOnBoard();
