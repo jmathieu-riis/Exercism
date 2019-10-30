aad874d5da2efd3a8512dc0d21ce7b8992970b2f
jmathieu-riis <jmathieu@riis.com>
2019-10-30 11:21:29 -0400

    Solved leap year problem

--- leap/src/main/java/Leap.java
+++ leap/src/main/java/Leap.java
@@ -1,7 +1,15 @@
 class Leap {
-
+    /**
+     * Purpose: Determine if given year is a leap year
+     * returns true on every year that is evenly divisible by 4
+     * returns false for every year that is evenly divisible by 100
+     * returns true if the year is evenly divisible by 400
+     * */
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        boolean divBy4 = year % 4 == 0;
+        boolean divBy100 = year % 100 == 0;
+        boolean divBy400 = year % 400 == 0;
+        return divBy400 || divBy4 && !divBy100;
     }
 
 }
